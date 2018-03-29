b703bdcf368f60174be00963511a5e02bd19fd85
moskomule <hataya@keio.jp>
2018-03-29 19:31:00 +0900

    fix: bash and utf-8 problem

--- mk_cuda8.sh
+++ mk_cuda8.sh
@@ -5,4 +5,6 @@ echo "FROM nvidia/cuda:8.0-cudnn6-devel-ubuntu16.04" > ./miniconda-gpu/8/Dockerf
 tail -n +2 ./miniconda-cpu/Dockerfile >> ./miniconda-gpu/8/Dockerfile
 
 echo "FROM moskomule/dockerfiles:cuda8" > ./server/8/Dockerfile
-tail -n +2 ./server/9/Dockerfile > ./server/8/Dockerfile
+tail -n +2 ./server/9/Dockerfile >> ./server/8/Dockerfile
+
+echo "finished!"
