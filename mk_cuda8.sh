41d7174b05619e289cba1198aec36407a573bccc
moskomule <hataya@keio.jp>
2018-03-29 14:24:03 +0900

    add server

--- mk_cuda8.sh
+++ mk_cuda8.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # semi-automatically update cuda8
-echo "FROM nvidia/cuda:8.0-cudnn6-devel-ubuntu16.04" > ./miniconda-gpu8/Dockerfile
-tail -n +2 ./miniconda-gpu/Dockerfile >> ./miniconda-gpu8/Dockerfile
+echo "FROM nvidia/cuda:8.0-cudnn6-devel-ubuntu16.04" > ./miniconda-gpu/8/Dockerfile
+tail -n +2 ./miniconda-cpu/Dockerfile >> ./miniconda-gpu/8/Dockerfile
+
+echo "FROM moskomule/dockerfiles:cuda8" > ./server/8/Dockerfile
+tail -n +2 ./server/9/Dockerfile > ./server/8/Dockerfile
